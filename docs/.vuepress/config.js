9bf9b3ab9dd3e1fd069883c8fcaf451a772007a4
Supachai Kheawjuy <bossthth1209mine@gmail.com>
2021-07-14 00:57:54 +0700

    add "nestedloop" to sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -95,7 +95,7 @@ module.exports = {
         children: [
           "week4/loop/while.md",      // 12 jul 2021
           "week4/loop/for.md",        // 13 jul 2021
-          // "week4/loop/nestedloop.md", // 14 jul 2021
+          "week4/loop/nestedloop.md", // 14 jul 2021
         ],
       },
     ],
